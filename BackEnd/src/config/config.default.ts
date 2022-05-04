713a35bd325ac6f556b624d9372fc26dfc411d60
“算子” <1687712633@qq.com>
2022-05-04 11:09:31 +0800

    初始化项目

---
+++ BackEnd/src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1651632777192_226',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
